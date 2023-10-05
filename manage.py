5f1205e406f4d18f9e4cd6870a32781a05f17bf4
serg <sergukr86@gmail.com>
2023-10-05 02:38:56 +0200

    reformat manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
@@ -11,15 +12,10 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            
             "available on your PYTHONPATH environment variable? Did you "
-            
             "forget to activate a virtual environment?"
-
         ) from exc
     execute_from_command_line(sys.argv)
-
-
 
 
 if __name__ == "__main__":
